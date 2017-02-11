bb06ed60a4d285c00f98807e716dcf2d4f8378a7
VaraPrasadLolugu <prasad35.mca@gmail.com>
2017-02-11 19:35:17 +0530

    Basic files

---
+++ src/main/java/com/groundwork/fms/FmsWebProviderApplication.java
@@ -0,0 +1,12 @@
+package com.groundwork.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsWebProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsWebProviderApplication.class, args);
+	}
+}
